53b435b4fc3fe74d9767955938b3c5f79f741f05
Maccio92 <valerio362@gmail.com>
2021-12-01 16:42:13 +0100

    add 3 grid

--- js/script.js
+++ js/script.js
@@ -5,64 +5,75 @@ con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro. */
 
-// Creo Campo Easy
-let row = 10;
-let col = 10;
 let fieldEasy = document.querySelector('.field');
-let numberSquareEasy = row * col;
-let numbersEasy = [];
-for (let i=1;i<=100;i++) {
-  numbersEasy.push(i);
-}
-// console.log(numbersEasy);
-for (let i = 0; i < numberSquareEasy; i++) {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    square.style.width = `calc(100% / ${col})`;
-    square.style.height = `calc(100% / ${row})`;
-    // console.log(square.style);
-    square.append(numbersEasy[i]);
-    fieldEasy.append(square);
-}
-
-// Creo Campo Medium
-let rowM = 9;
-let colM = 9;
 let fieldMedium = document.querySelector('.field');
-let numberSquareMedium = rowM * colM;
-let numbersMedium = [];
-for (let i = 1; i <= numberSquareMedium; i++) {
-  numbersMedium.push(i);
-}
-// console.log(numbersEasy);
-for (let i = 0; i < numberSquareMedium; i++) {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    square.style.width = `calc(100% / ${colM})`;
-    square.style.height = `calc(100% / ${rowM})`;
-    // console.log(square.style);
-    square.append(numbersMedium[i]);
-    fieldMedium.append(square);
-}
-
-// Creo Campo Hard
-let rowH = 7;
-let colH = 7;
 let fieldHard = document.querySelector('.field');
-let numberSquareHard = rowH * colH;
-let numbersHard = [];
-for (let i = 1; i <= numberSquareHard ; i++) {
-  numbersHard.push(i);
-}
-// console.log(numbersEasy);
-for (let i = 0; i < numberSquareHard; i++) {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    square.style.width = `calc(100% / ${colH})`;
-    square.style.height = `calc(100% / ${rowH})`;
-    // console.log(square.style);
-    square.append(numbersHard[i]);
-    fieldHard.append(square);
-}
+    
 
+let button = document.querySelector('.play');
+button.addEventListener ('click', function () {
+    let level = document.getElementById("diff");
+    let active = document.querySelector(".active");
+    let container = document.querySelector(".field");
 
+    if( level.value == 'easy'){
+        container.innerHTML = "";
+        active.classList.add("show");
+        let row = 10;
+        let col = 10;
+        let numberSquareEasy = row * col;
+        let numbersEasy = [];
+         for (let i = 1; i <= numberSquareEasy; i++) {
+        numbersEasy.push(i);
+        }
+        for (let i = 0; i < numberSquareEasy; i++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = `calc(100% / ${col})`;
+        square.style.height = `calc(100% / ${row})`;
+        // console.log(square.style);
+        square.append(numbersEasy[i]);
+        fieldEasy.append(square);
+        }
+    } else if ( level.value == 'medium'){
+        container.innerHTML = "";
+        active.classList.add("show");
+        let rowM = 9;
+        let colM = 9;
+        let numberSquareMedium = rowM * colM;
+        let numbersMedium = [];
+        for (let i = 1; i <= numberSquareMedium; i++) {
+             numbersMedium.push(i);
+        }
+    // console.log(numbersEasy);
+        for (let i = 0; i < numberSquareMedium; i++) {
+            let square = document.createElement('div');
+            square.classList.add('square');
+            square.style.width = `calc(100% / ${colM})`;
+            square.style.height = `calc(100% / ${rowM})`;
+            // console.log(square.style);
+            square.append(numbersMedium[i]);
+            fieldMedium.append(square);
+    }
+    } else {
+        container.innerHTML = "";
+        active.classList.add("show");
+        let rowH = 7;
+        let colH = 7;
+        let numberSquareHard = rowH * colH;
+        let numbersHard = [];
+        for (let i = 1; i <= numberSquareHard ; i++) {
+        numbersHard.push(i);    
+    }
+    // console.log(numbersEasy);
+        for (let i = 0; i < numberSquareHard; i++) {
+            let square = document.createElement('div');
+            square.classList.add('square');
+            square.style.width = `calc(100% / ${colH})`;
+            square.style.height = `calc(100% / ${rowH})`;
+            // console.log(square.style);
+            square.append(numbersHard[i]);
+            fieldHard.append(square);
+    }
+    }
+})
